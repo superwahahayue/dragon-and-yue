2f5eadc519d7b3e23bad8dc8f660d2340b4d73c2
Evan <1361851668@qq.com>
2023-06-22 17:03:19 +0800

    perf:提交

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
+## author Ben&&Evan
+
+## vue2+ts 的开发版本，此项目目前还不知道要干嘛，先写再说
+
 # vue01
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,10 @@
       <el-button type="primary">主要按钮</el-button>
       <el-button type="success">成功按钮</el-button>
       <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
+      <el-button type="warning">警告按</el-button>
+      <el-button type="danger"
+        >危险按钮(test`sssssssssssssssssssssssssssssssssssssssssssssssss`)</el-button
+      >
     </el-row>
   </div>
 </template>
